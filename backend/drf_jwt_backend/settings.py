1aaeaf4f6d44cb682817a894a4121260b5bdf549
ZacharyGoolman <zachary.goolman@gmail.com>
2022-04-29 17:55:47 -0400

    Fixed some syntax errors

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig'
+    'cars.apps.CarsConfig',
     'youtube_clone'  
     # This app is for example use only
 ]
