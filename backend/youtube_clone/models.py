f383692b966cfa5a8e718e558f7cb5b21d529e99
Austin <ofthevalley111@gmail.com>
2022-05-02 10:46:39 -0500

    created the comment and reply models

--- backend/youtube_clone/models.py
+++ backend/youtube_clone/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey()
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+
+class Reply(models.Model):
+    user = models.ForeignKey()
+    comment = models.ForeignKey()
+    text = CharField()
