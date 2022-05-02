6a98ff314b6a642da918b362714775705462e0a2
Austin <ofthevalley111@gmail.com>
2022-05-02 10:55:29 -0500

    created serializers for models Comment and Reply

---
+++ backend/youtube_clone/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Comment 
+from .models import Reply
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['user','video_id','text','likes','dislikes']
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['user','comment','text']
