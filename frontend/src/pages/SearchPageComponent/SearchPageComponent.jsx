aef3badbfaac6ed1e15fe9754997987fb1d33ea3
Austin <ofthevalley111@gmail.com>
2022-05-06 11:25:36 -0500

    changed className for map of SearchResults

--- frontend/src/pages/SearchPageComponent/SearchPageComponent.jsx
+++ frontend/src/pages/SearchPageComponent/SearchPageComponent.jsx
@@ -50,10 +50,10 @@ const handleClick = (event, id, title, description) => {
         <div className="searchResults">
         {/* Place searchBar component here */}
         <SearchBar getSearchResults={getSearchResults}/>
-            <div className="allRelatedVideos">
+            <div className="allSearchResults">
                 {searchResults.map(video =>(
                     <span>
-                        <div class = "relatedVideo">
+                        <div class = "SearchResult">
                         <input type="image" 
                                 onClick={(event) => handleClick(event, video.id.videoId, video.snippet.title, video.snippet.description)}
                                 src = {video.snippet.thumbnails.medium.url}
